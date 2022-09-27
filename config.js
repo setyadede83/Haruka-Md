0b899409c84862ec25b98c2f489fcc2d3438bf44
setyadede83 <114272572+setyadede83@users.noreply.github.com>
2022-09-27 11:43:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+{
+    "ownerNumber": ["6285659872989@s.whatsapp.net","6285659872989@s.whatsapp.net"],
+    "ownerName": "Adnan Boot Nice Try",
+    "instagram" : "https://instagram.com/Adnanboot_nt",
+    "botName": "Adnan-Bootོ ",
+    "footer": "api.zeeoneofc.xyz",
+    "sessionName": "session",
+    "pathimg": "./media/Haruka.jpg",
+    "BotKey": "Gsyt6jRJ",
+    "auto_welcomeMsg": true,
+    "auto_leaveMsg": true,    
+    "autobio": true,
+    "anticall": true,
+    "autorespond": false,
+    "autoblok212": true,
+    "autoread": true,
+    "gamewaktu": 90,
+    "limitCount": 25,
+    "gcount": {
+        "prem": 1000,
+        "user": 15
+    }
+}
